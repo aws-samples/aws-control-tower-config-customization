66654fafe1c756497fe92e35f7e9f8ad2aa7ad0e
vsr2158 <v.shekhar@yahoo.com>
2022-08-10 10:23:14 -0700

    First commit and move from internal to external repository.

---
+++ ct_configrecorder_override_consumer.py
@@ -0,0 +1,129 @@
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify,merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+
+import boto3
+import json
+import os
+import logging
+
+def lambda_handler(event, context):
+    
+    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    logging.getLogger().setLevel(LOG_LEVEL)
+
+    try:
+        
+        logging.info('Event Data:')
+        logging.info(event)
+
+        body = json.loads(event['Records'][0]['body'])
+        account_id = body['Account']
+        aws_region = body['Region']
+        event = body['Event']
+
+        logging.info(f'Extracted Account: {account_id}')
+        logging.info(f'Extracted Region: {aws_region}')
+        logging.info(f'Extracted Event: {event}')
+
+        STS = boto3.client("sts")
+
+        def assume_role(account_id, role='AWSControlTowerExecution'):
+            '''
+            Return a session in the target account using Control Tower Role
+            '''
+            try:
+                curr_account = STS.get_caller_identity()['Account']
+                if curr_account != account_id:
+                    part = STS.get_caller_identity()['Arn'].split(":")[1]
+
+                    role_arn = 'arn:' + part + ':iam::' + account_id + ':role/' + role
+                    ses_name = str(account_id + '-' + role)
+                    response = STS.assume_role(RoleArn=role_arn, RoleSessionName=ses_name)
+                    sts_session = boto3.Session(
+                        aws_access_key_id=response['Credentials']['AccessKeyId'],
+                        aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+                        aws_session_token=response['Credentials']['SessionToken']   )
+
+                    return sts_session
+            except ClientError as exe:
+                logging.error('Unable to assume role')
+                raise exe
+
+
+        sts_session = assume_role(account_id)
+        logging.info(f'Printing STS session: {sts_session}')
+
+        #Lets use the session and create a client for configservice
+        configservice = sts_session.client('config',region_name=aws_region)
+
+        #lets describe for configguration recorder
+        configrecorder = configservice.describe_configuration_recorders()
+        logging.info(f'Existing Configuration Recorder :' ,configrecorder)
+
+        #ControlTower created configuration recorder with name "aws-controltower-BaselineConfigRecorder" and we will update just that
+        try:
+            role_arn = 'arn:aws:iam::' + account_id + ':role/aws-controltower-ConfigRecorderRole'
+            CONFIG_RECORDER_RESOURCE_STRING = os.getenv('CONFIG_RECORDER_RESOURCE_LIST')
+            CONFIG_RECORDER_RESOURCE_LIST = CONFIG_RECORDER_RESOURCE_STRING.split(',')
+
+            # Event = Delete is when stack is deleted, we rollback changed made and leave it as ControlTower Intended
+            if event == 'Delete':
+                response = configservice.put_configuration_recorder(
+                ConfigurationRecorder={
+                    'name': 'aws-controltower-BaselineConfigRecorder',
+                    'roleARN': role_arn,
+                    'recordingGroup': {
+                        'allSupported': True,
+                        'includeGlobalResourceTypes': False
+                    }
+                })
+            else:
+                response = configservice.put_configuration_recorder(
+                ConfigurationRecorder={
+                    'name': 'aws-controltower-BaselineConfigRecorder',
+                    'roleARN': role_arn,
+                    'recordingGroup': {
+                        'allSupported': False,
+                        'includeGlobalResourceTypes': False,
+                        'resourceTypes': CONFIG_RECORDER_RESOURCE_LIST
+                    }
+                })
+
+            logging.info(f'Response for put_configuration_recorder :{response} ')
+
+            #lets describe for configuration recorder after the update
+            configrecorder =  configservice.describe_configuration_recorders()
+            logging.info(f'Post Change Configuration recorder : {configrecorder}')
+
+        except ClientError as exe:
+            logging.error('Unable to Update Config Recorder for Account and Region : ', account_id, aws_region)
+            configrecorder =  configservice.describe_configuration_recorders()
+            logging.info(f'Exception : {configrecorder}')
+            raise exe
+
+        return {
+            'statusCode': 200
+        }
+
+    except Exception as e:
+        exception_type = e.__class__.__name__
+        exception_message = str(e)
+        logging.exception(f'{exception_type}: {exception_message}')
---
+++ ct_configrecorder_override_producer.py
@@ -0,0 +1,146 @@
+
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify,merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+import boto3
+import cfnresource
+import os
+import logging
+
+def lambda_handler(event, context):
+    
+    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    logging.getLogger().setLevel(LOG_LEVEL)
+
+    try:
+        logging.info('Event Data: ')
+        logging.info(event)
+        sqs_url = os.getenv('SQS_URL')
+        excluded_accounts = os.getenv('EXCLUDED_ACCOUNTS')
+        logging.info(f'Excluded Accounts: {excluded_accounts}')
+        sqs_client = boto3.client('sqs')
+        
+        # Check if the lambda was trigerred from EventBridge.
+        # If so extract Account and Event info from the event data.
+        
+        is_eb_trigerred = 'source' in event
+        
+        logging.info(f'Is EventBridge Trigerred: {str(is_eb_trigerred)}')
+        event_source = ''
+        
+        if is_eb_trigerred:
+            event_source = event['source']
+            logging.info(f'Control Tower Event Source: {event_source}')
+            event_name = event['detail']['eventName']
+            logging.info(f'Control Tower Event Name: {event_name}')
+        
+        if event_source == 'aws.controltower' and event_name == 'UpdateManagedAccount':    
+            account = event['detail']['serviceEventDetails']['updateManagedAccountStatus']['account']['accountId']
+            logging.info(f'overriding config recorder for SINGLE account: {account}')
+            override_config_recorder(excluded_accounts, sqs_url, account, 'controltower')
+        elif event_source == 'aws.controltower' and event_name == 'CreateManagedAccount':  
+            account = event['detail']['serviceEventDetails']['createManagedAccountStatus']['account']['accountId']
+            logging.info(f'overriding config recorder for SINGLE account: {account}')
+            override_config_recorder(excluded_accounts, sqs_url, account, 'controltower')
+        elif event_source == 'aws.controltower' and event_name == 'UpdateLandingZone':
+            logging.info('overriding config recorder for ALL accounts due to UpdateLandingZone event')
+            override_config_recorder(excluded_accounts, sqs_url, '', 'controltower')
+        elif ('LogicalResourceId' in event) and (event['RequestType'] == 'Create'):
+            logging.info('CREATE CREATE')
+            logging.info(
+                'overriding config recorder for ALL accounts because of first run after function deployment from CloudFormation')
+            override_config_recorder(excluded_accounts, sqs_url, '', 'Create')
+            response = {}
+            ## Send signal back to CloudFormation after the first run
+            cfnresource.send(event, context, cfnresource.SUCCESS, response, "CustomResourcePhysicalID")
+        elif ('LogicalResourceId' in event) and (event['RequestType'] == 'Delete'):
+            logging.info('DELETE DELETE')
+            logging.info(
+                'overriding config recorder for ALL accounts because of first run after function deployment from CloudFormation')
+            override_config_recorder(excluded_accounts, sqs_url, '', 'Delete')
+            response = {}
+            ## Send signal back to CloudFormation after the final run
+            cfnresource.send(event, context, cfnresource.SUCCESS, response, "CustomResourcePhysicalID")
+        else:
+            logging.info("No matching event found")
+
+        logging.info('Execution Successful')
+        
+        # TODO implement
+        return {
+            'statusCode': 200
+        }
+
+    except Exception as e:
+        exception_type = e.__class__.__name__
+        exception_message = str(e)
+        logging.exception(f'{exception_type}: {exception_message}')
+
+
+def override_config_recorder(excluded_accounts, sqs_url, account, event):
+    
+    try:
+        client = boto3.client('cloudformation')
+        # Create a reusable Paginator
+        paginator = client.get_paginator('list_stack_instances')
+        
+        # Create a PageIterator from the Paginator
+        if account == '':
+            page_iterator = paginator.paginate(StackSetName ='AWSControlTowerBP-BASELINE-CONFIG')
+        else:
+            page_iterator = paginator.paginate(StackSetName ='AWSControlTowerBP-BASELINE-CONFIG', StackInstanceAccount=account)
+            
+        sqs_client = boto3.client('sqs')
+        for page in page_iterator:
+            logging.info(page)
+            
+            for item in page['Summaries']:
+                account = item['Account']
+                region = item['Region']
+                send_message_to_sqs(event, account, region, excluded_accounts, sqs_client, sqs_url)
+                    
+    except Exception as e:
+        exception_type = e.__class__.__name__
+        exception_message = str(e)
+        logging.exception(f'{exception_type}: {exception_message}')
+
+def send_message_to_sqs(event, account, region, excluded_accounts, sqs_client, sqs_url):
+    
+    try:
+
+        #Proceed only if the account is not excluded
+        if account not in excluded_accounts:
+        
+            #construct sqs message
+            sqs_msg = f'{{"Account": "{account}", "Region": "{region}", "Event": "{event}"}}'
+
+            #send message to sqs
+            response = sqs_client.send_message(
+            QueueUrl=sqs_url,
+            MessageBody=sqs_msg)
+            logging.info(f'message sent to sqs: {sqs_msg}')
+            
+        else:    
+            logging.info(f'Account excluded: {account}')
+                
+    except Exception as e:
+        exception_type = e.__class__.__name__
+        exception_message = str(e)
+        logging.exception(f'{exception_type}: {exception_message}')            
